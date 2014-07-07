17fb880ea0d3e08a9924b859e1dfed5ff5356d93
pongnguy <alfred.wechselberger@technologyhatchery.com>
2014-07-06 19:38:29 -0700

    IntelliJ minor changes3.

--- php/json.php
+++ php/json.php
@@ -4,7 +4,7 @@
  * User: Alfred
  * Date: 7/2/2014
  * Time: 10:31 PM
- ..........*/
+ ...............*/
 //$output = file_get_contents("../json/mobinsight/Survey.json");
 $output = file_get_contents("../json/mobinsight/Analytics.json");
 $json = json_decode($output);
